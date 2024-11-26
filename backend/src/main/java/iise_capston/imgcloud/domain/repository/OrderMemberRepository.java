4e3c6085ccaee884d5bb6e96ac6093550073fe59
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-11-26 20:19:35 +0900

    Delete backend/src/main/java/iise_capston/imgcloud/domain/repository/OrderMemberRepository.java

--- backend/src/main/java/iise_capston/imgcloud/domain/repository/OrderMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package iise_capston.imgcloud.domain.repository;
-
-import iise_capston.imgcloud.member.OrderMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderMemberRepository extends JpaRepository<OrderMember,Long> {
-}
