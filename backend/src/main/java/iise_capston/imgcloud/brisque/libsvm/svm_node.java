364373451bb7a2985e7f77ff9cde3177c82ee6ed
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-11-26 20:22:48 +0900

    Delete backend/src/main/java/iise_capston/imgcloud/brisque directory

--- backend/src/main/java/iise_capston/imgcloud/brisque/libsvm/svm_node.java
+++
@@ -1,7 +0,0 @@
-package iise_capston.imgcloud.brisque.libsvm;
-
-public class svm_node implements java.io.Serializable
-{
-	public int index;
-	public double value;
-}
--- backend/src/main/java/iise_capston/imgcloud/brisque/libsvm/svm_print_interface.java
+++
@@ -1,5 +0,0 @@
-package iise_capston.imgcloud.brisque.libsvm;
-public interface svm_print_interface
-{
-	public void print(String s);
-}
